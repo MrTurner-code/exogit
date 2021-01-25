358b73c1deacff45dd80af3ecad63c9eccd68e0a
Arnaud Bouteyre <>
2021-01-25 15:44:54 +0100

    no message

---
+++ verif.php
@@ -0,0 +1,21 @@
+<?php
+if($username !== "" && $password !== "")
+    {
+        $requete = "SELECT count(*) FROM utilisateur where 
+              nom_utilisateur = '".$username."' and mot_de_passe = '".$password."' ";
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['username'] = $username;
+           header('Location: principale.php');
+        }
+        else
+        {
+           header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+?>
